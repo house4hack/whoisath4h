a4d8dc6e629cff0c4d5cd96da61f7a7c408e968f
skullkey <schalk.heunis@gmail.com>
2013-01-14 10:00:47 +0200

    updated serverscripts

--- routerscript/forwardmac.sh
+++ routerscript/forwardmac.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 IPPATTERN="[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
+MACPATTERN="([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})"
 THIS="`ifconfig wlan0 | grep  -o -E "$IPPATTERN  B" | grep -o -E "$IPPATTERN"`"
-tcpdump -s 0 -i mon0 -e not host $THIS | grep -E -o ' (BSSID|DA|SA).[0-9a-f][0-9a-f]:[0-9a-f][0-9a-f]:[0-9a-f][0-9a-f]:[0-9a-f][0-9a-f]:[0-9a-f][0-9a-f]:[0-9a-f][0-9a-f] '  | nc -u 239.255.42.99 5555
+THISMAC="`ifconfig wlan0 | grep  -o -E "$MACPATTERN"`" 
+tcpdump -s 0 -i mon0 -nne "not host $THIS and (type data or type ctl) and not ether host $THISMAC and ether[0] & 1 = 0" | grep -E -o " (BSSID|DA|SA|TA|RA).$MACPATTERN "  | nc -u 239.255.42.99 5555
